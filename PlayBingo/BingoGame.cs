4e64572e9557d1362a918ec63602df1e1475c474
Andres Flores <floresc.andres@gmail.com>
2019-07-02 23:04:23 -0400

    The random works

---
+++ PlayBingo/BingoGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayBingo
+{
+    class BingoGame
+    {
+        List<int> numbersList = new List<int> { };
+
+        public int PlayGame()
+        {
+            Random bingoNumber = new Random();
+            var nums = Enumerable.Range(1, 75).OrderBy(x => bingoNumber.Next()).ToList();
+
+            while (numbersList.Count < 75)
+            {
+                numbersList.Add(nums[0]);
+                nums.RemoveAt(0);
+            }
+
+            foreach (int num in numbersList)
+            {
+                Console.WriteLine(num);
+            }
+            return numbersList.Count();
+        }
+    }
+}
---
+++ PlayBingo/BingoTesting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PlayBingo
+{
+    [TestFixture]
+    public class BingoTesting
+    {
+        BingoGame game = new BingoGame();
+
+        [Test]
+        public void ConfirmThatTheNumberIsBetweenOneAndSeventyFiveIclusive()
+        {
+            int largestValue = 75;
+            int lowestValue = 1;
+            int output = game.PlayGame();
+
+            Assert.GreaterOrEqual(largestValue, output);
+            Assert.LessOrEqual(lowestValue, output);
+        }
+
+        [Test]
+        public void CallCorrectNumbersMulitpleTimes()
+        {
+            int expected = 75;
+            int output = game.PlayGame();
+
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
